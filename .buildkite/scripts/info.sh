e6e6bd21ba9e9f45ddc4e5ad8fcb4f6bc9e59c6f
Hamidreza Ebtehaj <hamidreza.ebtehaj@gmail.com>
2022-07-18 22:28:03 +0200

    update info

--- .buildkite/scripts/info.sh
+++ .buildkite/scripts/info.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 set -o errexit
 
-echo "branch: $BUILDKITE_BRANCH"
+echo "git: "
+echo "   branch: $BUILDKITE_BRANCH"
+echo "   commit: $BUILDKITE_COMMIT"
+echo "   checkout path: $BUILDKITE_BUILD_CHECKOUT_PATH"
+echo "   author name: $BUILDKITE_BUILD_AUTHOR"
+echo "   author email: $BUILDKITE_BUILD_AUTHOR_EMAIL"
+
+echo "build: "
+echo "   id: $BUILDKITE_BUILD_ID"
+echo "   number: $BUILDKITE_BUILD_NUMBER"
+echo "   path: $BUILDKITE_BUILD_PATH"
+echo "   url: $BUILDKITE_BUILD_URL"
+echo "   command: $BUILDKITE_COMMAND"
+
+echo "artifacts: "
+echo "   paths: $BUILDKITE_ARTIFACT_PATHS"
+echo "   upload desitnation: $BUILDKITE_ARTIFACT_UPLOAD_DESTINATION"
+
+echo "github: "
+echo "   deployment id: $BUILDKITE_GITHUB_DEPLOYMENT_ID"
+echo "   deployment env: $BUILDKITE_GITHUB_DEPLOYMENT_ENVIRONMENT"
+echo "   task: $BUILDKITE_GITHUB_DEPLOYMENT_TASK"
+echo "   payload: $BUILDKITE_GITHUB_DEPLOYMENT_PAYLOAD"
