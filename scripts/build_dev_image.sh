6b89cbc4c2884770a401aa9fd7c35710f69e59bd
Hamidreza Ebtehaj <hamidreza.ebtehaj@gmail.com>
2022-07-19 04:04:26 +0200

    login in into docker

--- scripts/build_dev_image.sh
+++ scripts/build_dev_image.sh
@@ -9,6 +9,8 @@ else
   TAG=dev-$BUILDKITE_BUILD_NUMBER
 fi
 
+cat ~/.config/docker/keyfile.json | docker login -u _json_key --password-stdin https://eu.gcr.io
+
 docker build -t $SERVICE_NAME:$TAG -f ./dockerfiles/development/Dockerfile .
 docker tag $SERVICE_NAME:$TAG $IMAGE_REGISTRY_PATH/$SERVICE_NAME:$TAG  
 docker push $IMAGE_REGISTRY_PATH/$SERVICE_NAME:$TAG  
