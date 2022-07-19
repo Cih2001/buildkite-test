49a1035889af59ec0933de5042eea4fe7c142480
Hamidreza Ebtehaj <hamidreza.ebtehaj@gmail.com>
2022-07-19 13:39:00 +0200

    some refactoring

--- .buildkite/scripts/info.sh
+++ .buildkite/scripts/info.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # e	Exit script immediately if any command returns a non-zero exit status.
-# u	Exit script immediately if an undefined variable is used (for example, echo "$UNDEFINED_ENV_VAR").
 # o pipefail	Ensure Bash pipelines (for example, cmd | othercmd) return a non-zero status if any of the commands fail, rather than returning the exit status of the last command in the pipeline.
-set -euo pipefail
+set -eo pipefail
 
 source ./scripts/config.sh
 source ./.buildkite/scripts/common.sh
